d7b89d68d805a0190a334511241cf06c0c048033
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2022-08-17 17:29:17 +0100

    new changes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { theme } from './theme'
-import { ThemeProvider } from 'styled-components'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
