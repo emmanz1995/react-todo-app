d683731b0098290d15dccecebf75146f2ede1ef2
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2022-08-24 12:55:33 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Todo from './components/todo/todo'
 import './app.scss'
 import moonIcon from './images/icon-moon.svg'
