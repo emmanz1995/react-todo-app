e38902ecefdb322d7da314dc43de5c2f0e36e6db
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2022-12-15 22:31:46 +0000

    new changes made to ui

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -90,10 +90,3 @@ const Todo = () => {
 }
 
 export default Todo
-
-
- {/*<div className="attribution">*/}
-{/*    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend*/}
-{/*    Mentor</a>.*/}
-{/*    Coded by <a href="https://github.com/emmanz1995" target="_blank">emmanz95</a>.*/}
-{/*</div>*/}
